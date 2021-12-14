d783078db668bf9e254899c575abf025dd954d8f
tuanbkc4 <tuanbachkhoadn@gmail.com>
2021-12-14 10:27:34 +0700

    validate model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
+    get "static_pages/home"
+    get "static_pages/shop"
+    get "static_pages/about"
+    get "static_pages/contact"
+    get "static_pages/cart"
   end
 end
